cb99846ddca90b2ea2e3fc935906adc316e89c4e
Trevor Kanten <75340205+tkanten@users.noreply.github.com>
2021-03-31 19:19:09 -0600

    Create startup.sh

---
+++ Bash/startup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#this setup is good for x86
+#change permissions to 777
+
+sudo apt-get update -y
+sudo apt-get upgrade -y 
+
+#wireshark requires an additional prompt for capture permissions
+sudo apt-get install wireshark -y 
+
+#below this line is fully automatic
+sudo apt-get install gcc -y
+sudo apt-get install make -y
+sudo apt-get install gdb -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install net-tools -y
+sudo apt-get install nasm -y
+sudo apt-get install ld -y
+sudo apt-get install as -y
+sudo apt-get install tree -y
+sudo apt-get install bless -y
+sudo apt-get install htop -y
+sudo apt install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y
+sudo apt-get install gnu-arm-linux-gnueabihf -y
+sudo apt-get install yersinia -y
+
+
+#gef install
+wget -O ~/.gdbinit-gef.py -q http://gef.blah.cat/py
+echo source ~/.gdbinit-gef.py > ~/.gdbinit
+#echo "set disassembly-flavor intel" >> ~/.gdbinit
+echo "gef config context.show_registers_raw True" >> ~/.gdbinit
+#additional python modules for gef commands
+pip install keystone-engine
+pip install unicorn
+pip install capstone
+pip install ropper
+
+
+
+#install snap for pycharm (for debian based distros)
+sudo apt-get install snapd -y
+sudo systemctl enable --now snapd.socket
+sudo snap refresh
+
+#installing pycharm
+sudo snap install pycharm-community --classic
+
+#some versions of linux don't include these packages, needed for pycharm venv
+sudo apt-get install python3-distutils -y
+sudo apt-get install python3-apt -y
+
+#create a permanent env variable for snap
+export PATH=$PATH:/snap/bin
+#run pycharm with "pycharm-community" in terminal
