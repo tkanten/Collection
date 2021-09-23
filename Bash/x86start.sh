26b229bc807e2f229707f01d7a85fa528427b63a
Trevor Kanten <75340205+tkanten@users.noreply.github.com>
2021-09-23 14:15:41 -0600

    Update x86start.sh

--- Bash/x86start.sh
+++ Bash/x86start.sh
@@ -32,20 +32,6 @@ sudo apt-get install ascii -y
 sudo apt-get install openssh-client -y
 #sudo apt-get install openssh-server -y
 sudo apt-get install sshpass -y
-#better cat
-sudo apt-get install bat
-#re-assign executable name from batcat to bat
-mkdir -p ~/.local/bin
-ln -s /usr/bin/batcat ~/.local/bin/bat
-
-
-#sudo apt install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y
-#sudo apt-get install gcc-arm* -y # 2GB
-
-
-#apache2
-#sudo apt-get install apache2 -y
-#sudo service apache2 stop
 
 #gef install
 wget -O ~/.gdbinit-gef.py -q https://github.com/hugsy/gef/raw/master/gef.py
@@ -53,35 +39,53 @@ echo source ~/.gdbinit-gef.py > ~/.gdbinit
 echo "set disassembly-flavor intel" >> ~/.gdbinit
 echo "gef config context.show_registers_raw True" >> ~/.gdbinit
 
-#optional python modules for gef commands
+
+## OPTIONALS
+
+# optional python modules for gef commands
 #pip install keystone-engine
 #pip install unicorn
 #pip install capstone
 #pip install ropper
 
 
-#systems without snapd installed below
+# better version of cat - best done manually
+#sudo apt-get install bat
+#mkdir -p ~/.local/bin
+#ln -s /usr/bin/batcat ~/.local/bin/bat
 
-#install snap for pycharm (for debian based distros)
+# cross-compiling libs
+#sudo apt install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu -y
+#sudo apt-get install gcc-arm* -y # 2GB
+
+
+# apache2
+#sudo apt-get install apache2 -y
+#sudo service apache2 stop
+
+
+# systems without snapd installed below
+
+# install snap for pycharm (for debian based distros)
 #sudo apt-get install snapd -y
 #sudo systemctl enable --now snapd.socket
 #sudo snap refresh
 
-#installing pycharm
+# installing pycharm
 #sudo snap install pycharm-community --classic
 
 #apparmor required to prevent pycharm-community picked up as a priv escalation false-positive
 #sudo apt-get install apparmor
 #sudo systemctl enable --now apparmor.service
 
-#some versions of linux don't include these packages, needed for pycharm venv
+# some versions of linux don't include these packages, needed for pycharm venv
 #sudo apt-get install python3-distutils -y
 #sudo apt-get install python3-apt -y
 
 
-#installing visual studio code
+# installing visual studio code
 #sudo snap install --classic code
 
-#create a permanent env variable for snap
+# create a permanent env variable for snap
 #export PATH=$PATH:/snap/bin
 #run pycharm with "pycharm-community" in terminal
