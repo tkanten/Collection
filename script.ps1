9baf0b0c960f9f66c503703f7d065d575bff53c5
Trevor Kanten <75340205+tkanten@users.noreply.github.com>
2022-03-13 22:11:02 -0600

    upd

--- script.ps1
+++ script.ps1
@@ -2,41 +2,45 @@
 
 $divider = "--------------------------------------------------------------------"
 
+Function Divider {
+    Param ($file)
+    echo "--------------------------------------------------------------------" >> $file
+}
 
 Function Get-BasicInfo {
-$basic_info = "basic_info.txt"
-echo "System Info:" > $basic_info
-systeminfo | findstr /B /C:'OS NAME' /C:'OS Version' >> $basic_info
+    $basic_info = "basic_info.txt"
+    echo "System Info:" > $basic_info
+    systeminfo | findstr /B /C:'OS NAME' /C:'OS Version' >> $basic_info
 
-echo $divider >> $basic_info
+    Divider $basic_info
 
-echo "Hostname:" >> $basic_info
-hostname >> $basic_info
+    echo "Hostname:" >> $basic_info
+    hostname >> $basic_info
 
-echo $divider >> $basic_info
+    Divider $basic_info
 
-echo "Current User:" >> $basic_info
-echo $env:UserName >> $basic_info
+    echo "Current User:" >> $basic_info
+    echo $env:UserName >> $basic_info
 
-echo $divider >> $basic_info
+    Divider $basic_info
 
-echo "Path:" >> $basic_info
-echo $env:path >> $basic_info
+    echo "Path:" >> $basic_info
+    echo $env:path >> $basic_info
 
-echo $divider >> $basic_info
+    Divider $basic_info
 
-echo "Users:" >> $basic_info
-net users >> $basic_info
+    echo "Users:" >> $basic_info
+    net users >> $basic_info
 
-echo $divider >> $basic_info
+    Divider $basic_info
 
-echo "Administrators:" >> $basic_info
-net localgroup Administrators >> $basic_info
+    echo "Administrators:" >> $basic_info
+    net localgroup Administrators >> $basic_info
 
-echo $divider >> $basic_info
+    Divider $basic_info
 
-echo "RDP Users:" >> $basic_info
-net localgroup "Remote Desktop Users" >> $basic_info
+    echo "RDP Users:" >> $basic_info
+    net localgroup "Remote Desktop Users" >> $basic_info
 }
 
 Get-BasicInfo
