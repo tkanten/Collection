7b8b5e8e56bab6d479e1662f15c71bc21f2f7e6e
Trevor Kanten <75340205+tkanten@users.noreply.github.com>
2022-03-13 23:01:13 -0600

    added task sched enum

--- script.ps1
+++ script.ps1
@@ -1,19 +1,21 @@
 # Basic info
 $dump_file = "infodump.txt"
 
-Function SectionHeader([string]$SectionName) {
-    echo "==============================================================" >> $dump_file
-    echo "SECTION $SectionName" >> $dump_file
-    echo "==============================================================" >> $dump_file
-
+Function SectionHeader {
+    Param ($Section_Name)
+    echo "================================================================================================" >> $dump_file
+    echo "=== $Section_Name" >> $dump_file
+    echo "================================================================================================" >> $dump_file
 }
 
 Function SubDivider {
-    echo "++=++=++=++=++=++=++=++=++=++=++=++=++=++=++=++=++=++=++=++=++" >> $dump_file
+    echo "=----------------------------------------------------------------------------------------------=" >> $dump_file
 }
 
 Function Get-BasicInfo {
-    echo "System Info:" > $dump_file
+    SectionHeader("BASIC INFO")
+
+    echo "System Info:" >> $dump_file
     systeminfo | findstr /B /C:'OS NAME' /C:'OS Version' >> $dump_file
 
     SubDivider
@@ -45,12 +47,12 @@ Function Get-BasicInfo {
 
     echo "RDP Users:" >> $dump_file
     net localgroup "Remote Desktop Users" >> $dump_file
-
-    SubDivider
 }
 
 Function Get-NetworkInfo {
-    echo "Network Information:" > $dump_file
+    SectionHeader "NETWORK INFORMATION"
+
+    echo "Network Information:" >> $dump_file
     ipconfig /all >> $dump_file
 
     SubDivider
@@ -67,26 +69,37 @@ Function Get-NetworkInfo {
 
     netstat -ano >> $dump_file
 
-    SubDivider
-
 }
 
 Function Get-FirewallEnum {
+    SectionHeader "FIREWALL ENUMERATION"
 
-    echo "Firewall State:"
+    echo "Firewall State:" >> $dump_file
     netsh firewall show state >> $dump_file
 
     SubDivider $dump_file
 
     echo "Firewall Config:" >> $dump_file
-    netsh firewall show config
+    netsh firewall show config >> $dump_file
 }
 
+Function Get-TaskSchedEnum {
+    SectionHeader "WINDOWS TASKS AND SCHEDULED PROCESS ENUMERATION"
 
-SectionHeader("Basic-Info")
+    echo "Tasks:" >> $dump_file
+    schtasks /query /fo LIST /v >> $dump_file
+
+    SubDivider
+
+    echo "Running Process:" >> $dump_file
+    tasklist /SVC >> $dump_file
+}
+
+echo "Made by Trevor Kanten" > $dump_file
 Get-BasicInfo
-#Get-NetworkInfo
-#Get-FirewallEnum
+Get-NetworkInfo
+Get-FirewallEnum
+Get-TaskSchedEnum
 
 
 
