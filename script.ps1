c8dcd475861c35474ef0d42d54794a280ce15629
Trevor Kanten <75340205+tkanten@users.noreply.github.com>
2022-03-13 21:46:32 -0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,22 @@
 # Basic info
+
+$divider = "--------------------------------------------------------------------"
 $basic_info = "basic_info.txt"
 
-echo "System Info:" >> basic_info.txt
+
+echo "System Info:" >> $basic_info
 systeminfo | findstr /B /C:'OS NAME' /C:'OS Version' >> $basic_info
+
+echo $divider >> $basic_info
+
+echo "Hostname:" >> $basic_info
+hostname >> $basic_info
+
+echo $divider >> $basic_info
+
+echo "Current User:" >> $basic_info
+echo %username% >> $basic_info
+
+echo $divider >> $basic_info
+
 
