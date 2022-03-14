fe8ac26bfdf7706a4891d6853ff41f1a69596e9c
Trevor Kanten <75340205+tkanten@users.noreply.github.com>
2022-03-13 21:42:00 -0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,1 +1,6 @@
-systeminfo | findstr /B /C:'OS NAME' /C:'OS Version' >> basic_info.txt
+# Basic info
+$basic_info = "basic_info.txt"
+
+echo "System Info:" >> basic_info.txt
+systeminfo | findstr /B /C:'OS NAME' /C:'OS Version' >> $basic_info
+
